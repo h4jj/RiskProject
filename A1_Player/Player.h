9adfd6b72829ac028774736c85635619736cab8e
Hajj <1999ahmadelhajj@gmail.com>
2022-02-12 12:07:06 -0500

    Updated files

--- A1_Player/Player.h
+++ A1_Player/Player.h
@@ -1,15 +1,12 @@
 #pragma once
 #include <iostream>
 
-
-
 class Player {
-private:
-    
-public:
-    Player();
-    ~Player();
-    void toDefend();
-    void toAttack();
-    void issueOrder();
+    public:
+        int value;
+        
+    public:
+        Player();
+        Player(int val);
+        int getValue();
 };
