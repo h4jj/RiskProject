2d5e4db0e89b872322d1950b1262df5f0e39c52f
mariemek <59948102+mariemek@users.noreply.github.com>
2022-04-03 13:53:46 -0400

    Update PlayerStrategies.cpp

---
+++ GameEngine/PlayerStrategies.cpp
@@ -0,0 +1,137 @@
+
+#include "PlayerStrategies.h"
+//
+//PlayerStrategy
+//
+
+//constructor
+PlayerStrategy::PlayerStrategy(){}
+
+//destructor
+PlayerStrategy::~PlayerStrategy(){}
+
+//copy constructor
+PlayerStrategy::PlayerStrategy(const PlayerStrategy& ps) {
+	std::cout << "Copy constructor successfully called" << std::endl;
+    this->StrategyName = ps.StrategyName;
+    }
+
+// Assignment Operator
+PlayerStrategy& PlayerStrategy::operator=(const PlayerStrategy& ps) {
+    std::cout << "Assignment operator successfully called" << std::endl;
+    this->StrategyName = ps.StrategyName;
+    return *this;
+}
+
+//
+//Human
+//
+
+//constructor
+HumanPlayerStrategy::HumanPlayerStrategy(){}
+
+//destructor
+HumanPlayerStrategy::~HumanPlayerStrategy(){}
+
+//copy constructor
+HumanPlayerStrategy::HumanPlayerStrategy(const HumanPlayerStrategy& hs) {
+	std::cout << "Copy constructor successfully called" << std::endl;
+    //player = hs.player;
+    }
+    
+// Assignment Operator
+HumanPlayerStrategy& HumanPlayerStrategy::operator=(const HumanPlayerStrategy& hs) {
+    std::cout << "Assignment operator successfully called" << std::endl;
+   // player =hs.player;
+    return *this;
+}
+
+//
+//Aggressive
+//
+
+//constructor
+AggressivePlayerStrategy::AggressivePlayerStrategy(){}
+
+//destructor
+AggressivePlayerStrategy::~AggressivePlayerStrategy(){}
+
+//copy constructor
+AggressivePlayerStrategy::AggressivePlayerStrategy(const AggressivePlayerStrategy& as) {
+	std::cout << "Copy constructor successfully called" << std::endl;
+    //player = as.player;
+    }
+    
+// Assignment Operator
+AggressivePlayerStrategy& AggressivePlayerStrategy::operator=(const AggressivePlayerStrategy& as) {
+    std::cout << "Assignment operator successfully called" << std::endl;
+    //player = as.player;
+    return *this;
+}
+
+//
+//Benevolent
+//
+
+//constructor
+BenevolentPlayerStrategy::BenevolentPlayerStrategy(){}
+
+//destructor
+BenevolentPlayerStrategy::~BenevolentPlayerStrategy(){}
+
+//copy constructor
+BenevolentPlayerStrategy::BenevolentPlayerStrategy(const BenevolentPlayerStrategy& bs) {
+	std::cout << "Copy constructor successfully called" << std::endl;
+    //player = bs.player;
+    }
+    
+// Assignment Operator
+BenevolentPlayerStrategy& BenevolentPlayerStrategy::operator=(const BenevolentPlayerStrategy& bs) {
+    std::cout << "Assignment operator successfully called" << std::endl;
+    //player = bs.player;
+    return *this;
+}
+//
+//Neutral
+//
+
+//constructor
+NeutralPlayerStrategy::NeutralPlayerStrategy(){}
+
+//destructor
+NeutralPlayerStrategy::~NeutralPlayerStrategy(){}
+
+//copy constructor
+NeutralPlayerStrategy::NeutralPlayerStrategy(const NeutralPlayerStrategy& ns) {
+	std::cout << "Copy constructor successfully called" << std::endl;
+    //player = ns.player;
+    }
+    
+// Assignment Operator
+NeutralPlayerStrategy& NeutralPlayerStrategy::operator=(const NeutralPlayerStrategy& ns) {
+    std::cout << "Assignment operator successfully called" << std::endl;
+    //player = ns.player;
+    return *this;
+}
+//
+//Cheater
+//
+
+//constructor
+CheaterPlayerStrategy::CheaterPlayerStrategy(){}
+
+//destructor
+CheaterPlayerStrategy::~CheaterPlayerStrategy(){}
+
+//copy constructor
+CheaterPlayerStrategy::CheaterPlayerStrategy(const CheaterPlayerStrategy& cs) {
+	std::cout << "Copy constructor successfully called" << std::endl;
+    //player = cs.player;
+    }
+    
+// Assignment Operator
+CheaterPlayerStrategy& CheaterPlayerStrategy::operator=(const CheaterPlayerStrategy& cs) {
+    std::cout << "Assignment operator successfully called" << std::endl;
+    //player = cs.player;
+    return *this;
+}
